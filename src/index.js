63c2d874c192c850c6eb4ddf4748b6426a2108e6
Young-Gaius <youngehu8@gmail.com>
2023-07-21 23:50:59 +0100

    second commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ const tasks = [
     { description: 'Task 1', completed: false, index: 1 },
     { description: 'Task 2', completed: true, index: 2 },
     { description: 'Task 3', completed: false, index: 3 },
-    
   ];
   
   function renderTasks() {
